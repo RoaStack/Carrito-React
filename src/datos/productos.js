58fdaf69a13463e2150670a72d854b909941f262
GustavoXCODE <gustavo.munoz23@icloud.com>
2025-07-24 20:09:28 -0400

    agregando 5 productos mas en las cards

--- src/datos/productos.js
+++ src/datos/productos.js
@@ -4,7 +4,11 @@ const productos = [
   { id: 3, nombre: 'RestFull-rolls', precio: 25000, imagen: 'RestFull-rolls.jpg' },
   { id: 4, nombre: 'DevOps-delight', precio: 24000, imagen: 'DevOps-delight.jpg' },
   { id: 5, nombre: 'Express-set', precio: 20000, imagen: 'Express-set.jpg' },
-
+  { id: 6, nombre: 'Laravel-Lunchbox', precio: 12000, imagen: 'Mongo-mix.jpg' },
+  { id: 7, nombre: 'Combo-Flexbox', precio: 13000, imagen: 'Node-nigiri.jpg' },
+  { id: 8, nombre: 'Sushi-Grid', precio: 21000, imagen: 'RestFull-rolls.jpg' },
+  { id: 9, nombre: 'CSS-Combo', precio: 24000, imagen: 'DevOps-delight.jpg' },
+  { id: 10, nombre: 'Webhook-Wasabi', precio: 28000, imagen: 'Express-set.jpg' },
 ];
 
 export default productos;
